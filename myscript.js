53b8ae5769e86ab1218e6380a756ebdc8a197f90
Kristijan Turic <kristijan.turic1@gmail.com>
2023-02-03 12:58:04 +0700

    Added buttons for clearing the grid and changing the grid size

--- myscript.js
+++ myscript.js
@@ -1,4 +1,6 @@
-const gridSize = 64;
+//#region Making a starting container and grid 16x16
+
+var gridSize = 16;
 
 // Creating the container
 let container = document.createElement('div');
@@ -11,16 +13,22 @@ container.style.height = "500px";
 container.style.outline = "solid";
 container.style.alignSelf = "center";
 
-document.body.appendChild(container);
+let content = document.getElementById("content");
+content.appendChild(container);
+
+
 
 // Creating the elements for the container (cells)
 let div = document.createElement('div');
 div.className = "grid";
 
-// Divide the side of the container with the gridSize to get the number side of the small square in the container
-div.style.width = div.style.height = (500/gridSize).toString() + "px" ;
-
 div.style.backgroundColor = 'white';
+
+MakeNewGrid(gridSize);
+
+//#endregion
+
+//#region Drawing on the grid
 
 // Hover effect over element (doesn't get erased automatically)
 
@@ -30,17 +38,13 @@ let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-console.log(container.addEventListener('mouseover', function (event) {
+container.addEventListener('mouseover', function (event) {
     if (mouseDown)
     {
         event.target.style.backgroundColor = 'lightblue';
     }
     
-}));
-
-
-
-
+});
 
 /* Erases the color
 container.addEventListener('mouseout', function(event) {
@@ -48,11 +52,75 @@ container.addEventListener('mouseout', function(event) {
 })
 */
 
-// Appending the appropriate number of children to the container
-for(let i = 0; i < gridSize; i++)
-{
-    for(let j = 0; j < gridSize; j++)
+//#endregion
+
+//#region UI Functionality
+
+// Button for clearing the drawing area (grid)
+let btnClear = document.getElementById("btnClear");
+
+btnClear.addEventListener('click', function() {
+    RemoveAllChildren();
+    MakeNewGrid(gridSize);
+});
+
+// Button that asks for input and changes gridSize to the input
+let btnGrid = document.getElementById("btnGrid");
+
+btnGrid.addEventListener('click', function() {
+    let input = prompt("Enter the desired grid size:");
+    if (inputCheck(input))
     {
-        container.appendChild(div.cloneNode(true));
+        gridSize = Number(input);
+        console.log(gridSize);
+
+        RemoveAllChildren();
+        MakeNewGrid(gridSize);
+    }
+    
+});
+
+//#endregion
+
+//#region Helper Methods
+
+// Function to check if the input from the button is correct and in range[1 - 64]
+function inputCheck(input)
+{
+    if (Number(input) < 1 || Number(input) > 64 || isNaN(Number(input))) 
+    {
+        alert("The input has to be a number between 1 and 64!");
+        return false;
+    }
+    else
+    {
+        return true;
     }
 }
+
+// Removes all the children from the container
+function RemoveAllChildren()
+{
+    while (container.firstChild)
+    {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// Adds new children to the container when the gridSize changes
+function MakeNewGrid(gridSize)
+{
+    // Divide the side of the container with the gridSize to get the number side of the small square in the container
+    div.style.width = div.style.height = (500/gridSize).toString() + "px" ;
+    
+    // Appending the appropriate number of children to the container
+    for(let i = 0; i < gridSize; i++)
+    {
+        for(let j = 0; j < gridSize; j++)
+        {
+            container.appendChild(div.cloneNode(true));
+        }
+    }
+}
+
+//#endregion
